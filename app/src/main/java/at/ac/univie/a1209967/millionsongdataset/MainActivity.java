b6c3928b51115baff66432fe15d7cab0c8127276
Adrian <a1209967@unet.univie.ac.at>
2016-05-02 18:02:22 +0200

    initial commit;

---
+++ app/src/main/java/at/ac/univie/a1209967/millionsongdataset/MainActivity.java
@@ -0,0 +1,13 @@
+package at.ac.univie.a1209967.millionsongdataset;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
