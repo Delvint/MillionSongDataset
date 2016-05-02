b67a21bb728008f1e497599a2422c5d0a80b4828
Adrian <a1209967@unet.univie.ac.at>
2016-05-02 19:21:40 +0200

    implemented basic music player; instructions of implementsation in readme;

---
+++ app/src/main/java/at/ac/univie/a1209967/millionsongdataset/Management.java
@@ -0,0 +1,7 @@
+package at.ac.univie.a1209967.millionsongdataset;
+
+/**
+ * Created by Adlbert on 02.05.2016.
+ */
+public class Management {
+}
---
+++ app/src/main/java/at/ac/univie/a1209967/millionsongdataset/entities/Song.java
@@ -0,0 +1,20 @@
+package at.ac.univie.a1209967.millionsongdataset.entities;
+
+/**
+ * Created by Adlbert on 02.05.2016.
+ */
+public class Song {
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
